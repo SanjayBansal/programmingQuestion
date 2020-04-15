e94c5389ff4a073a5203f3e42b01c4156db67c00
sanjay <wb-sanjayban568087@antfin.com>
2020-04-15 20:30:07 +0530

    linkedList

---
+++ src/main/java/linkedList/CreatingAndTraversingLinkedList_01.java
@@ -0,0 +1,23 @@
+package linkedList;
+
+public class CreatingAndTraversingLinkedList_01 {
+    public static Node head;
+    public static void main(String args[]){
+        head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+        //Assign nodes
+        head.next = second;
+        second.next = third;
+        //Print linked nodes
+        printNode(head);
+    }
+
+    private static void printNode(Node head) {
+        Node n = head;
+        while(n != null){
+            System.out.println(n.data);
+            n = n.next;
+        }
+    }
+}
